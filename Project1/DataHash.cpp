b5d01ce66c8a5af75acb4411fd085a191d913068
Conormacpherson <Cmacpherson1766@conestogac.on.ca>
2019-02-16 12:44:12 -0500

    Add hashing class

---
+++ Project1/DataHash.cpp
@@ -0,0 +1,89 @@
+#include "DataHash.hpp"
+
+#pragma warning (disable:4996)
+
+
+std::string DataHash::MD5HashData(const char* dataString) {
+
+	unsigned char digest[MD5_DIGEST_LENGTH];
+
+	MD5((unsigned char*)dataString, strlen(dataString), digest);
+
+	char mdString[MD5_OUTPUT_SIZE];
+
+	for (int i = 0; i < MD5_HALF_HASH_SIZE; i++) {
+
+		sprintf(&mdString[i * 2], "%02x", digest[i]);
+	}
+
+	return std::string{ mdString };
+}
+
+
+std::string DataHash::MD5HashData(const std::string& dataString) {
+
+	if (dataString.empty()) {
+
+		return dataString;
+	}
+
+	unsigned char digest[MD5_DIGEST_LENGTH] = {0};
+
+	MD5((unsigned char *)dataString.c_str(), dataString.size(), digest);
+
+	char mdString[MD5_OUTPUT_SIZE];
+
+	for (int i = 0; i < MD5_HALF_HASH_SIZE; i++) {
+	
+		sprintf(&mdString[i * 2], "%02x", digest[i]);
+	}
+
+	return std::string{mdString};
+}
+
+
+std::stringstream DataHash::MD5HashData(const std::stringstream& dataSStream) {
+
+	if (dataSStream.str().empty()) {
+
+		return std::stringstream{};
+	}
+
+	unsigned char digest[MD5_DIGEST_LENGTH] = { 0 };
+
+	MD5((unsigned char *)dataSStream.str().c_str(), dataSStream.str().size(), digest);
+
+	char mdString[MD5_OUTPUT_SIZE];
+
+	for (int i = 0; i < MD5_HALF_HASH_SIZE; i++) {
+
+		sprintf(&mdString[i * 2], "%02x", digest[i]);
+	}
+
+	return std::stringstream{ mdString };
+}
+
+
+
+std::vector<unsigned char> DataHash::MD5HashData(const std::vector<unsigned char>& dataVector) {
+
+	if (dataVector.empty()) {
+
+		return dataVector;
+	}
+
+	unsigned char MD5digest[MD5_DIGEST_LENGTH] = { 0 };
+
+	MD5((unsigned char *)&dataVector[0], dataVector.size(), MD5digest);
+
+	char mdString[MD5_OUTPUT_SIZE];
+
+	for (int i = 0; i < MD5_HALF_HASH_SIZE; i++) {
+
+		sprintf(&mdString[i * 2], "%02x", MD5digest[i]);
+	}
+
+	std::vector<unsigned char> hashVector(mdString, mdString + MD5_OUTPUT_SIZE - 1);
+
+	return hashVector;
+}
---
+++ Project1/DataHash.hpp
@@ -0,0 +1,24 @@
+#ifndef DATAHASH_HPP
+#define DATAHASH_HPP
+
+
+#include <openssl/md5.h>
+#include <string>
+#include <sstream>
+#include <vector>
+
+constexpr int MD5_OUTPUT_SIZE = 33;
+constexpr int MD5_HALF_HASH_SIZE = 16;
+
+class DataHash
+{
+	public:
+
+		std::string static MD5HashData(const char* dataString);
+		std::string static MD5HashData(const std::string& dataString);
+		std::stringstream static MD5HashData(const std::stringstream& dataSStream);
+		std::vector<unsigned char> static MD5HashData(const std::vector<unsigned char>& dataVector);
+};
+
+
+#endif /* DATAHASH_HPP */
